68486f7b883b4520926155fc528f545027352e3d
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2024-01-27 02:56:55 -0500

    More code regression, will refactor later

--- frontend/components/timeline.tsx
+++ frontend/components/timeline.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import TimelineItem from "./timelineItem";
+import TimelineDot from "./timelineDot";
 
 interface TimelineProps {
   items: (string | number | null)[][];
@@ -12,14 +13,10 @@ export default function Timeline({ items }: TimelineProps) {
       <div className="flex">
         {items.map((item, index) => (
           <div key={index} className="flex flex-col gap-4 justify-center">
-            {/* <div className="flex">
-              <div className="w-40 h-1 bg-transparent"></div>
-              <TimelineItem title={item[0]} year={item[1]} />
-            </div> */}
             <div className="flex items-center">
-              <div className="flex w-40 h-1 bg-blue-500"></div>
-              <div className="w-6 h-6 bg-blue-500 rounded-full "></div>
-              <div className="flex flex-grow h-1 bg-blue-500"></div>
+              {/* <div className="flex w-40 h-1 bg-blue-500"></div> */}
+              <TimelineDot item={item} />
+              {/* <div className="flex flex-grow h-1 bg-blue-500"></div> */}
             </div>
           </div>
         ))}
--- frontend/components/timelineDot.tsx
+++ frontend/components/timelineDot.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import TimelineItem from "./timelineItem";
 
 interface TimelineDotProps {
   item: (string | number | null)[];
@@ -13,11 +14,34 @@ export default function TimelineDot({ item }: TimelineDotProps) {
   const handleMouseLeave = () => {
     setIsHovered(false);
   };
+  const dotClassName = `w-6 h-6 rounded-full ${
+    isHovered ? "bg-[#484A57]" : "bg-blue-500"
+  }`;
+  const timelineItemStyle = {
+    position: "absolute",
+    left: "50%", // Same x position as the dot
+    top: "-130px", // Adjust this value based on your requirements
+    transform: "translateX(-50%)", // Center the item above the dot
+    width: "100%", // Full width
+    textAlign: "center", // Center the content horizontally
+    // display: isHovered ? "block" : "none",
+  };
   return (
-    <div
-      className="w-6 h-6 rounded-full ${isHovered ? 'bg-red-500' : 'bg-blue-500'}"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    />
+    <div className="relative w-full">
+      <div className="flex items-center">
+        <div className="flex w-40 h-1 bg-blue-500"></div>
+        <div
+          className={dotClassName}
+          onMouseEnter={handleMouseEnter}
+          // onMouseLeave={handleMouseLeave}
+        />
+        <div className="flex flex-grow h-1 bg-blue-500"></div>
+      </div>
+      {isHovered && (
+        <div style={timelineItemStyle}>
+          <TimelineItem title={item[0]} year={item[1]} />
+        </div>
+      )}
+    </div>
   );
 }
