5904d4334b2c7283a8a034371a538b8a59c93c27
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2024-01-26 23:27:47 -0500

    Implement auth0 from Okta

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,26 @@
+"use client";
 import Image from "next/image";
 import Sidebar from "@/components/sidebar";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+  const router = useRouter();
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+  if (!user && !isLoading) {
+    router.push("/api/auth/login");
+    return null;
+  }
+
   return (
-    <>
+    user && (
       <main className="">
         <Sidebar />
       </main>
-    </>
+    )
   );
 }
