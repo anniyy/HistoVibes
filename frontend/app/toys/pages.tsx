0b14bede0ef87f52c00faa7b261a5364078a9a44
Verusins <115052147+Verusins@users.noreply.github.com>
2024-01-26 22:03:20 -0500

    Everything until working on toys
    As titile

---
+++ frontend/app/toys/pages.tsx
@@ -0,0 +1,9 @@
+export default function Toys() {
+  return (
+    <>
+      <div className="flex flex-row w-40 h-4/5">
+        <div className="flex flex-column"></div>
+      </div>
+    </>
+  );
+}
