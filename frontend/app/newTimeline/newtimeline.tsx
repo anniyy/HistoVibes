800069714df6c0785690407ade53c059da3500fc
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2024-01-28 02:11:00 -0500

    Make timeline scrollable

--- frontend/app/newTimeline/newtimeline.tsx
+++ frontend/app/newTimeline/newtimeline.tsx
@@ -72,7 +72,7 @@ export default function NewTimelinePage({
   }, [topics]);
 
   return (
-    <div className="flex items-center">
+    <div className="flex items-center overflow-y-auto">
       <Timeline items={topics}></Timeline>
       <div className="flex items-end h-screen">
         {hasTimelines && (
